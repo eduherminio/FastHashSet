8427bf04134f5da7a19868134d6009b4e736affe
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2020-12-26 16:18:49 +0100

    Add IEnumerable extension .ToFastHashSet() (#3)

---
+++ FastHashSet/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Motvin.Collections;
+using System.Collections.Generic;
+
+namespace FastHashSet
+{
+    public static class EnumerableExtensions
+    {
+        public static FastHashSet<T> ToFastHashSet<T>(this IEnumerable<T> enumerable)
+        {
+            return new FastHashSet<T>(enumerable);
+        }
+    }
+}

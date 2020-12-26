ad0178d310d4f04dbbab2f52d5addd2b238d88b6
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-26 22:18:09 +0100

    Comment most benchmarks

--- HashSetBench/Program.cs
+++ HashSetBench/Program.cs
@@ -2,13 +2,14 @@
 using BenchmarkDotNet.Running;
 
 BenchmarkRunner.Run<RefCountHashSetVsFastHashSet>();
-BenchmarkRunner.Run<SumSmallClassVsStructList>();
-BenchmarkRunner.Run<AddSmallClassVsStructList>();
-BenchmarkRunner.Run<AddSmallClassVsStructFast3>();
-BenchmarkRunner.Run<AddSmallClassVsStructFast>();
-BenchmarkRunner.Run<AddSmallClassVsStructFast2>();
-BenchmarkRunner.Run<MinMaxIntRangeContains1to100>();
-BenchmarkRunner.Run<MinMaxIntRangeContains100to1_000>();
-BenchmarkRunner.Run<MinMaxIntRangeContains1_000to10_000>();
-BenchmarkRunner.Run<MinMaxIntRangeContains10_000to100_000>();
-BenchmarkRunner.Run<MinMaxIntRangeContains100_000to1_000_000>();
+
+// BenchmarkRunner.Run<SumSmallClassVsStructList>();
+// BenchmarkRunner.Run<AddSmallClassVsStructList>();
+// BenchmarkRunner.Run<AddSmallClassVsStructFast3>();
+// BenchmarkRunner.Run<AddSmallClassVsStructFast>();
+// BenchmarkRunner.Run<AddSmallClassVsStructFast2>();
+// BenchmarkRunner.Run<MinMaxIntRangeContains1to100>();
+// BenchmarkRunner.Run<MinMaxIntRangeContains100to1_000>();
+// BenchmarkRunner.Run<MinMaxIntRangeContains1_000to10_000>();
+// BenchmarkRunner.Run<MinMaxIntRangeContains10_000to100_000>();
+// BenchmarkRunner.Run<MinMaxIntRangeContains100_000to1_000_000>();
